69baffa97f386ddc558084f72b4ef8c82355bb17
Ashton Prigge <aprigge@library.ucla.edu>
2020-05-26 16:12:39 -0700

    VBT-90: Added check for document id name in normalizeCharacters().

--- jscripts/normalizeCharacters.js
+++ jscripts/normalizeCharacters.js
@@ -1,21 +1,30 @@
 function normalizeCharacters()
 	{
 		// Find searcArg and replace smart quote with single quote 
-		var search = document.getElementById("searchArg").value;
-		search = search.replace("\u2019", "'");
-		document.getElementById("searchArg").value = search;
+		if (document.getElementById("searchArg")){	
+			var search = document.getElementById("searchArg").value;
+			search = search.replace("\u2019", "'");
+			document.getElementById("searchArg").value = search;
+		}
 		
 		//Advanced search box has three searchArgs
-		var search1 = document.getElementById("searchArg1").value;
-		search1 = search1.replace("\u2019", "'");
-		document.getElementById("searchArg1").value = search1;
+		if (document.getElementById("searchArg1")){
+			var search1 = document.getElementById("searchArg1").value;
+			search1 = search1.replace("\u2019", "'");
+			document.getElementById("searchArg1").value = search1;
+		}
 
-		var search2 = document.getElementById("searchArg2").value;
-		search2 = search2.replace("\u2019", "'");
-		document.getElementById("searchArg2").value = search2;
+		if (document.getElementById("searchArg2")){
+			var search2 = document.getElementById("searchArg2").value;
+			search2 = search2.replace("\u2019", "'");
+			document.getElementById("searchArg2").value = search2;
+		}
 
-		var search3 = document.getElementById("searchArg3").value;
-		search3 = search3.replace("\u2019", "'");
-		document.getElementById("searchArg3").value = search3;
+		if (document.getElementById("searchArg3")){
+			var search3 = document.getElementById("searchArg3").value;
+			search3 = search3.replace("\u2019", "'");
+			document.getElementById("searchArg3").value = search3;
+		}
 	}
+
 
