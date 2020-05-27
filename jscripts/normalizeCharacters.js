c70740059c66ecf0b2e5f7c63246b06a12e8ccd9
Ashton Prigge <aprigge@library.ucla.edu>
2020-05-27 09:03:17 -0700

    VBT-90: normalizeCharacters changed to iterate over searchArgs.

--- jscripts/normalizeCharacters.js
+++ jscripts/normalizeCharacters.js
@@ -4,12 +4,12 @@ function normalizeCharacters()
 	var searchArgs = ["searchArg", "searchArg1", "searchArg2", "searchArg3"];
 	searchArgs.forEach(normalize);
 	function normalize (searchArg) {
-	if (document.getElementById(searchArg)){  
-       	var search = document.getElementById(searchArg).value;
-	   	search = search.replace("\u2019", "'");
-      	document.getElementById(searchArg).value = search;
+		if (document.getElementById(searchArg)){  
+    	   	var search = document.getElementById(searchArg).value;
+	   		search = search.replace("\u2019", "'");
+      		document.getElementById(searchArg).value = search;
+		 }
 	 }
-   }
 }
 
 
