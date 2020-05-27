a52848bc85dbc1d22e961f4087ff42f88240d3af
Ashton Prigge <aprigge@library.ucla.edu>
2020-05-27 09:17:37 -0700

    VBT-90: Fixed formatting in normalizeCharacters.

--- jscripts/normalizeCharacters.js
+++ jscripts/normalizeCharacters.js
@@ -5,9 +5,9 @@ function normalizeCharacters()
 	searchArgs.forEach(normalize);
 	function normalize (searchArg) {
 		if (document.getElementById(searchArg)){  
-    	   	var search = document.getElementById(searchArg).value;
-	   		search = search.replace("\u2019", "'");
-      		document.getElementById(searchArg).value = search;
+			var search = document.getElementById(searchArg).value;
+			search = search.replace("\u2019", "'");
+			document.getElementById(searchArg).value = search;
 		 }
 	 }
 }
