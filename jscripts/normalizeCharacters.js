ec7547f8f9bdb8778594f28522a32cadb31ac7ed
Ashton Prigge <aprigge@library.ucla.edu>
2020-05-27 08:57:57 -0700

    VBT-90: normalizeCharacters changed to iterate over searchArgs.

--- jscripts/normalizeCharacters.js
+++ jscripts/normalizeCharacters.js
@@ -1,30 +1,15 @@
 function normalizeCharacters()
-	{
-		// Find searcArg and replace smart quote with single quote 
-		if (document.getElementById("searchArg")){	
-			var search = document.getElementById("searchArg").value;
-			search = search.replace("\u2019", "'");
-			document.getElementById("searchArg").value = search;
-		}
-		
-		//Advanced search box has three searchArgs
-		if (document.getElementById("searchArg1")){
-			var search1 = document.getElementById("searchArg1").value;
-			search1 = search1.replace("\u2019", "'");
-			document.getElementById("searchArg1").value = search1;
-		}
-
-		if (document.getElementById("searchArg2")){
-			var search2 = document.getElementById("searchArg2").value;
-			search2 = search2.replace("\u2019", "'");
-			document.getElementById("searchArg2").value = search2;
-		}
-
-		if (document.getElementById("searchArg3")){
-			var search3 = document.getElementById("searchArg3").value;
-			search3 = search3.replace("\u2019", "'");
-			document.getElementById("searchArg3").value = search3;
-		}
-	}
+{
+	// Find searcArg and replace smart quote with single quote 
+	var searchArgs = ["searchArg", "searchArg1", "searchArg2", "searchArg3"];
+	searchArgs.forEach(normalize);
+	function normalize (searchArg) {
+	if (document.getElementById(searchArg)){  
+       	var search = document.getElementById(searchArg).value;
+	   	search = search.replace("\u2019", "'");
+      	document.getElementById(searchArg).value = search;
+	 }
+   }
+}
 
 
