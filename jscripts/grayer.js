cf84acea5fdf05cd21dc4f2f808fc61e2f2bba94
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2020-06-16 16:05:50 -0700

    VBT-1605: Added check for form existence. (#4)

--- jscripts/grayer.js
+++ jscripts/grayer.js
@@ -26,36 +26,40 @@ function grayer()
         var frm = document.getElementById("searchBasic");
 //alert("selected index = " + frm.searchCode.selectedIndex + "; qlimit = " + qlimit );
 //qlimit is not being maintained between calls
-	switch (frm.searchCode.selectedIndex) 
-	{
-		case 0:
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 14:
-			if (frm.setLimit.disabled == true) 
-			{
-				frm.setLimit.selectedIndex = qlimit;
-				frm.setLimit.disabled = false;
-			}
-			break;
-		case 6:
-		case 7:
-		case 8:
-		case 9:
-		case 10:
-		case 11:
-		case 12:
-		case 13:
-			if (frm.setLimit.disabled == false) 
-			{
-				qlimit = frm.setLimit.selectedIndex;
-				frm.setLimit.disabled = true;
-			}
-			frm.setLimit.selectedIndex = 0; // so it shows limit of All Material
-			break;
+	if (frm) 
+	{ 
+		switch (frm.searchCode.selectedIndex) 
+		{
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 14:
+				if (frm.setLimit.disabled == true) 
+				{
+					frm.setLimit.selectedIndex = qlimit;
+					frm.setLimit.disabled = false;
+				}
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			case 11:
+			case 12:
+			case 13:
+				if (frm.setLimit.disabled == false) 
+				{
+					qlimit = frm.setLimit.selectedIndex;
+					frm.setLimit.disabled = true;
+				}
+				frm.setLimit.selectedIndex = 0; // so it shows limit of All Material
+				break;
+		}
 	}
 }
+
 
